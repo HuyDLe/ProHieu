18046b113eec701e0845ef5a7b617da21b781bb0
HUY D LE <34200507+HuyDLe@users.noreply.github.com>
2020-02-21 13:59:46 -0800

    Update Homepage.java

--- app/src/main/java/com/example/prohieu/Homepage.java
+++ app/src/main/java/com/example/prohieu/Homepage.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
+////////////////////////////////////////////////////////////////////////////
 public class Homepage extends AppCompatActivity {
     private ImageButton buttonMaps;
 
