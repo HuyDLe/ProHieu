1833073832ca37a28ec4a92dbe0e36a7538f3103
Hieu Nguyen <hieukn1@uci.edu>
2020-02-21 13:35:17 -0800

    Initial commit

---
+++ app/src/main/java/com/example/prohieu/Homepage.java
@@ -0,0 +1,32 @@
+package com.example.prohieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Homepage extends AppCompatActivity {
+    private ImageButton buttonMaps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        buttonMaps = findViewById(R.id.map);
+        buttonMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linktoMapboxNavigation();
+            }
+        });
+    }
+
+    public void linktoMapboxNavigation() {
+        Intent intent = new Intent(this, MapboxNavigation.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/prohieu/NavigationDrawer.java
@@ -0,0 +1,188 @@
+package com.example.prohieu;
+
+import android.app.Dialog;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class NavigationDrawer extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser ;
+    Dialog popAddPost ;
+    private static final int PReqCode = 2 ;
+    private static final int REQUESCODE = 2 ;
+    ImageView popupUserImage,popupPostImage,popupAddBtn;
+    TextView popupTitle,popupDescription;
+    ProgressBar popupClickProgress;
+    private Uri pickedImgUri = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // ini
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        // ini popup
+        //iniPopup();
+        //setupPopupImageClick();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        updateNavHeader();
+    }
+
+    /*private void addPost(Post post) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Posts").push();
+        // get post unique ID and upadte post key
+        String key = myRef.getKey();
+        post.setPostKey(key);
+
+        // add post data to firebase database
+        myRef.setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                showMessage("Post Added successfully");
+                popupClickProgress.setVisibility(View.INVISIBLE);
+                popupAddBtn.setVisibility(View.VISIBLE);
+                popAddPost.dismiss();
+            }
+        });
+    }*/
+
+    private void showMessage(String message) {
+        Toast.makeText(NavigationDrawer.this,message,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.nav_home) {
+            //getSupportActionBar().setTitle("Home");
+            //getSupportFragmentManager().beginTransaction().replace(R.id.container,new HomeFragment()).commit();
+        } else if (id == R.id.nav_profile) {
+            //getSupportActionBar().setTitle("Profile");
+            //getSupportFragmentManager().beginTransaction().replace(R.id.container,new ProfileFragment()).commit();
+
+        } else if (id == R.id.nav_settings) {
+            //getSupportActionBar().setTitle("Settings");
+            //getSupportFragmentManager().beginTransaction().replace(R.id.container,new SettingsFragment()).commit();
+        }
+        else if (id == R.id.nav_signout) {
+            //FirebaseAuth.getInstance().signOut();
+            //Intent loginActivity = new Intent(getApplicationContext(),LoginActivity.class);
+            //startActivity(loginActivity);
+            //finish();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void updateNavHeader() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = headerView.findViewById(R.id.nav_username);
+        TextView navUserMail = headerView.findViewById(R.id.nav_user_mail);
+        ImageView navUserPhot = headerView.findViewById(R.id.nav_user_photo);
+        navUserMail.setText(currentUser.getEmail());
+        navUsername.setText(currentUser.getDisplayName());
+        // now we will use Glide to load user image
+        // first we need to import the library
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(navUserPhot);
+    }
+}
+
